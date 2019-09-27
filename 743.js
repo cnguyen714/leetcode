72f18462836151c5f68effe4fc8d34693e540d8f
Tony Nguyen <calvindnguyen714@gmail.com>
2019-09-26 21:34:22 -0700

    Working soln 743

--- 743.js
+++ 743.js
@@ -8,7 +8,7 @@
  */
 var networkDelayTime = function (times, N, K) {
   let maxTimeElapsed = 0;
-  let traversedSet = new Set([]);
+  let traversedSet = new Set([K]);
   let nodesTime = {};
   let weights = {};
   let queue = [K];
@@ -29,22 +29,30 @@ var networkDelayTime = function (times, N, K) {
     if (nodesTime[iter] === undefined) nodesTime[iter] = 0;
 
     let el;
-    for (let i = 0; i < weights[iter].length; i++) {
-      el = weights[iter][i];
+    for (let i = 0; weights[iter] && i < weights[iter].length; i++) {
+      let el = weights[iter][i];
+      let travelTime = nodesTime[iter] + el[2];
       if (nodesTime[el[1]] === undefined) {
-        nodesTime[el[1]] = nodesTime[iter] + el[2];
+        nodesTime[el[1]] = travelTime;
         traversedSet.add(el[1]);
-        if (maxTimeElapsed < nodesTime[el[1]]) maxTimeElapsed = nodesTime[el[1]];
+        // if (maxTimeElapsed < nodesTime[el[1]]) maxTimeElapsed = nodesTime[el[1]];
+        queue.push(el[1]);
+      } else if (travelTime < nodesTime[el[1]]){
+        nodesTime[el[1]] = travelTime;
+        traversedSet.add(el[1]);
+        // if (maxTimeElapsed < nodesTime[el[1]]) maxTimeElapsed = nodesTime[el[1]];
+        queue.push(el[1]);
       }
-      queue.push(el[1]);
     }
   }
 
   if (traversedSet.size === N) {
-    return maxTimeElapsed;
+    return Math.max(...Object.values(nodesTime));
   } else {
     return -1;
   }
 };
 
 console.log(networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2)); // 2
+console.log(networkDelayTime([[1, 2, 1], [2, 1, 3]], 2, 2)); // 3
+console.log(networkDelayTime([[1, 2, 1], [2, 3, 2], [1, 3, 4]], 3, 1)); // 3
