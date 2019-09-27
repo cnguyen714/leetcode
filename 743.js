6d43b63efb85dd681a1742932dd2ad769083edb0
Tony Nguyen <calvindnguyen714@gmail.com>
2019-09-26 21:10:27 -0700

    Add traversal

--- 743.js
+++ 743.js
@@ -9,7 +9,37 @@
 var networkDelayTime = function (times, N, K) {
   let maxTimeElapsed = 0;
   let traversedSet = new Set([]);
-  console.log(traversedSet.size);
+  let nodesTime = {};
+  let weights = {};
+  let queue = [K];
+
+  let last;
+  while (times.length !== 0) {
+    last = times.pop();
+    if (weights[last[0]] === undefined) {
+      weights[last[0]] = [last];
+    } else {
+      weights[last[0]].push(last);
+    }
+  }
+
+  let iter;
+  while(queue.length !== 0) {
+    iter = queue.shift();
+    if (nodesTime[iter] === undefined) nodesTime[iter] = 0;
+
+    let el;
+    for (let i = 0; i < weights[iter].length; i++) {
+      el = weights[iter][i];
+      if (nodesTime[el[1]] === undefined) {
+        nodesTime[el[1]] = nodesTime[iter] + el[2];
+        traversedSet.add(el[1]);
+        if (maxTimeElapsed < nodesTime[el[1]]) maxTimeElapsed = nodesTime[el[1]];
+      }
+      queue.push(el[1]);
+    }
+  }
+
   if (traversedSet.size === N) {
     return maxTimeElapsed;
   } else {
