f1ff07416ff851bf4882520a6411fff1db69ee70
Tony Nguyen <calvindnguyen714@gmail.com>
2019-10-02 13:55:35 -0700

    setup loop

--- 207.js
+++ 207.js
@@ -6,5 +6,20 @@
  * @return {boolean}
  */
 var canFinish = function (numCourses, prerequisites) {
+  let prereqsTo = {};
+  let prereqs = {};
+  let taken = [];
 
+  for (let i = 0; i < prerequisites.length; i++) {
+    let pair = prerequisites[i];
+
+    if (prereqs[pair[0]] === undefined) {
+      prereqs[pair[0]].push(pair[1]);
+    }
+    prereqsTo[pair[1]].push(pair[0]);
+  }
+
+
+
+  return taken.length === numCourses;
 };
