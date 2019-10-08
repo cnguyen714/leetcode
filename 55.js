583e4c0a60bd7a5b1e656bfdd4a47753cacc135c
Tony Nguyen <calvindnguyen714@gmail.com>
2019-10-07 18:18:10 -0700

    Solve 55

--- 55.js
+++ 55.js
@@ -4,5 +4,22 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
+  if (nums.length === 1) return true;
+  // let maxes = {};
+  let max = nums[0];
+  // maxes[0] = nums[0];
 
+  let it = 1;
+  while (it <= max) {
+    let currentMax = nums[it] + it;
+    if (currentMax > max) max = currentMax;
+    if (max >= nums.length - 1) return true;
+    // maxes[it] = max;
+    it += 1;
+  }
+
+  return false;
 };
+
+console.log(canJump([2,3,1,1,4])); // true
+console.log(canJump([3,2,1,0,4])); // false
