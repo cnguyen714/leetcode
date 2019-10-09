e9d6a40f9945e4598b019ccc9104899ff390c0ee
Tony Nguyen <calvindnguyen714@gmail.com>
2019-10-09 16:54:13 -0700

    Init 303

--- 55.js
+++ 55.js
@@ -6,16 +6,13 @@
 var canJump = function (nums) {
   if (nums.length === 1) return true;
   // let maxes = {};
-  let max = nums[0];
+  let reach = nums[0];
   // maxes[0] = nums[0];
 
-  let it = 1;
-  while (it <= max) {
-    let currentMax = nums[it] + it;
-    if (currentMax > max) max = currentMax;
-    if (max >= nums.length - 1) return true;
-    // maxes[it] = max;
-    it += 1;
+  for (let i = 1; i <= reach; i++) {
+    reach = Math.max(reach, nums[i] + i)
+    if (reach >= nums.length - 1) return true;
+    // maxes[i] = max;
   }
 
   return false;
