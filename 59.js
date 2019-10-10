e561c761e7f42403d3499eb32541a79cf2826f72
Tony Nguyen <calvindnguyen714@gmail.com>
2019-10-09 18:12:53 -0700

    Skeleton loop

--- 59.js
+++ 59.js
@@ -5,5 +5,34 @@
  * @return {number[][]}
  */
 var generateMatrix = function (n) {
+  if (n === 1) return [[1]];
+  let matrix = [];
 
+  for (let i = 0; i < n; i++) {
+    matrix.push(Array(n));
+  }
+
+  let top = left = 0;
+  let bottom = right = n;
+  let pos = [0, 0];
+  let val = 1;
+  
+  let dir = 0;
+  let direction = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1]
+  ];
+
+  while (pos[0] >= left &&
+         pos[0] < right &&
+         pos[1] >= top &&
+         pos[1] < bottom) {
+
+  }
+  
+
+
+  return matrix;
 };
