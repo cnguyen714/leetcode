bca7384301013b31290bb5030c3edcc4e9d99f8a
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-11 20:08:42 -0800

    Init 3

---
+++ 3.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  sub = {}
+
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
