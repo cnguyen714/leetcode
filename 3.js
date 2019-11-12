ef6a5049f4f21faec71123e924d017a32d0f634c
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-11 20:36:46 -0800

    Solve 3 unoptimally

--- 3.js
+++ 3.js
@@ -4,8 +4,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  sub = {}
+  let sub = {};
+  let max = 0;
+  let length = 0;
 
+  for (let i = 0; i < s.length; i++) {
+    if (sub[s[i]]) {
+      i = sub[s[i]];
+      sub = {};
+    } else {
+      sub[s[i]] = i;
+    }
+
+    length = Object.keys(sub).length;
+    if (length > max) {
+      max = length;
+    };
+  }
+  return max;
 };
 
 console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("dvdf")); // 3
