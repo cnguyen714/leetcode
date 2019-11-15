2430788c9c22f488f199302fd2d1db2a1be795a2
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-14 22:57:33 -0800

    Init product array without division

---
+++ etc/11-13whiteboarding.js
@@ -0,0 +1,17 @@
+/*
+write some function prodArray that takes in an array of numbers and returns an 
+array such that each index of the new array represents the product of all 
+numbers in the original array EXCLUDING the number at that index in the 
+original array.
+
+This should be solved in O(n) time without the use of division.  
+*/
+
+function prodArray(input) {
+
+}
+
+console.log(prodArray([]));
+console.log(prodArray([1,2,3]));
+console.log(prodArray([0,0,3]));
+console.log(prodArray([0,2,3]));
