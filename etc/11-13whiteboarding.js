e19ac8023bae8d01c2b271aaceb4936376b5b178
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-15 01:31:59 -0800

    Skeleton for product array

--- etc/11-13whiteboarding.js
+++ etc/11-13whiteboarding.js
@@ -8,7 +8,32 @@ This should be solved in O(n) time without the use of division.
 */
 
 function prodArray(input) {
+  let zero = [];
+  let output = [];
+  let left = [];
+  let right = [];
 
+  for (let i = input.length - 1; i > 0 && zero.length < 2; i--) {
+
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    if (zero.length > 1) {
+      ouput.push(0);
+    } else {
+      if (zero.length === 1) {
+        if (i === zero[0]) {
+          
+        } else {
+          ouput.push(0);
+        }
+      } else {
+
+      }
+    }
+  }
+
+  return output;
 }
 
 console.log(prodArray([]));
