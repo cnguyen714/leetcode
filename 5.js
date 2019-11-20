c2ef2575e9af037e2a922ba9926b2410b82660f4
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-19 23:55:07 -0800

    Partial 5

--- 5.js
+++ 5.js
@@ -6,9 +6,47 @@
  */
 var longestPalindrome = function (s) {
   let max = 0;
+  let newMax = 0;
   let out = "";
-  for (let i = 0; i < s.length; i++) {
+  let extend = 1;
+  let even = false
+  
+  for (let i = 0; i < s.length - 1; i++) {
+    extend = 1;
+
+    while ( s[i - extend] === s[i + extend] && 
+            i - extend >= 0 && 
+            i + extend < s.length ) {
     
+      newMax = 1 + extend * 2;
+      if (newMax > max) {
+        max = newMax;
+        out = s.substr(i - extend, i + extend);
+      }
+
+      extend++;
+    }
+
+    if(s[i] === s[i+1]) {
+      newMax = 2;
+      if (newMax > max) {
+        max = newMax;
+        out = s.substr(i, i + 2);
+      }
+
+      while ( s[i - extend] === s[i + 1 + extend] &&
+              i - extend >= 0 &&
+              i + 1 + extend < s.length ) {
+
+        newMax = 2 + extend * 2;
+        if (newMax > max) {
+          max = newMax;
+          out = s.substr(i - extend, i + extend);
+        }
+
+        extend++;
+      }
+    }
   }
 
   return out;
