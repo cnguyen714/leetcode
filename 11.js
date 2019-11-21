d43379fef8536256cf4fd244e9fbd44b08a0b192
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-20 21:11:52 -0800

    Solve 11

--- 11.js
+++ 11.js
@@ -5,5 +5,18 @@
  * @return {number}
  */
 var maxArea = function (height) {
+  let i = 0;
+  let j = height.length - 1;
+  let max = Math.min(height[i], height[j]) * (j - i);
 
+  while (i < j) {
+    height[i] <= height[j] ? i++ : j--;
+    max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+  }
+
+  return max;
 };
+
+console.log(maxArea([1,3]));
+console.log(maxArea([2,5,5]));
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
