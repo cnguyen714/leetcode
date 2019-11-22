c7b1fa1a2699b2c8096bbf5905caee6d80b22a2a
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-21 19:22:01 -0800

    Solve 17 through subsets

--- 17.js
+++ 17.js
@@ -5,27 +5,30 @@
  */
 
 const digitsCombo = {
-  "2": "abc",
-  "3": "def",
-  "4": "ghi",
-  "5": "jkl",
-  "6": "mno",
-  "7": "pqrs",
-  "8": "tuv",
-  "9": "wxyz",
+  "2": ["a", "b", "c"],
+  "3": ["d", "e", "f"],
+  "4": ["g", "h", "i"],
+  "5": ["j", "k", "l"],
+  "6": ["m", "n", "o"],
+  "7": ["p", "q", "r", "s"],
+  "8": ["t", "u", "v"],
+  "9": ["w", "x", "y", "z"],
 };
 
 var letterCombinations = function (digits) {
   if(digits === "") return [];
-  if(i >= digits.length) return [];
 
-  let out = [];
-  let digit = digits[i];
-  let letters = digitsCombo[digit];
-  // console.log(letters);
-  letters.split('').map(c => {
-    out.concat(letterCombinations(digits, i + 1).map(combo => c + combo));
-  });
+  let out = digitsCombo[digits[0]];
+  
+  for (let i = 1; i < digits.length; i++) {
+    let newArr = [];
+
+    digitsCombo[digits[i]].forEach(char => {
+      newArr.push(...out.map(combo => combo + char))
+    })
+
+    out = newArr;
+  }
 
   return out;
 };
