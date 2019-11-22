0b305290aa50cc1ac966bb500616297068bcf5b8
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-21 18:19:51 -0800

    Init 17

---
+++ 17.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+
+};
