48f716597a6fa0c6f70108f8c5e4dd70647823f7
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-21 19:00:52 -0800

    Abandon attempt of recursive 17

--- 17.js
+++ 17.js
@@ -3,6 +3,31 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function (digits) {
 
+const digitsCombo = {
+  "2": "abc",
+  "3": "def",
+  "4": "ghi",
+  "5": "jkl",
+  "6": "mno",
+  "7": "pqrs",
+  "8": "tuv",
+  "9": "wxyz",
 };
+
+var letterCombinations = function (digits) {
+  if(digits === "") return [];
+  if(i >= digits.length) return [];
+
+  let out = [];
+  let digit = digits[i];
+  let letters = digitsCombo[digit];
+  // console.log(letters);
+  letters.split('').map(c => {
+    out.concat(letterCombinations(digits, i + 1).map(combo => c + combo));
+  });
+
+  return out;
+};
+
+console.log(letterCombinations("23"));
