d75cf65f08b1b614c83d47dc9a649b686bcf18d1
Tony Nguyen <calvindnguyen714@gmail.com>
2019-11-25 23:52:42 -0800

    Solve 19 one pass

--- 19.js
+++ 19.js
@@ -22,37 +22,57 @@ function ListNode(val, node = null) {
 var removeNthFromEnd = function (head, n) {
   let nth = head;
   let prev = null;
-  let it = null;
+  let it = head;
+  let c = n;
 
-  while(n > 0) {
+  while(c > 0) {
     it = it.next;
-    n--;
+    c--;
   }
 
-  while(it != null) {
+  while(it) {
     it = it.next;
     prev = nth;
     nth = nth.next;
+    // console.log(`${prev ? prev.val : null}:${nth.val} - ${it ? it.val : null}`)
   }
 
-  if(it === head) {
-    head = it.next;
+
+  if(prev) {
+    if(nth) {
+      prev.next = nth.next;
+    } else {
+      prev.next = null;
+    }
   } else {
-    prev.next = next;
+    if(nth) {
+      head = nth.next;
+    } else {
+      head = null;
+    }
   }
+
+
 
   return head;
 };
 
-let e = new Node(5);
-let d = new Node(4,e);
-let c = new Node(3,d);
-let b = new Node(2,c);
-let a = new Node(1,b);
+let e = new ListNode(5);
+let d = new ListNode(4,e);
+let c = new ListNode(3,d);
+let b = new ListNode(2,c);
+let a = new ListNode(1,b);
 
 let it = a;
 
-while(it != null) {
+// while(it != null) {
+//   console.log(it.val);
+//   it = it.next;
+// }
+
+it = removeNthFromEnd(d, 2);
+
+while (it != null) {
   console.log(it.val);
   it = it.next;
 }
