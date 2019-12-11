a01d302f71d3bc61aa25fe0ea3e584efe7551ee0
Tony Nguyen <calvindnguyen714@gmail.com>
2019-12-11 00:05:25 -0800

    Skeleton first loop 21

--- 21.js
+++ 21.js
@@ -12,5 +12,34 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
+  let i = l1;
+  let j = l2;
+  let mergedRoot = null;
+  let listPos = null;
 
+  while (i != null && j != null) {
+    if(i.val <= j.val) {
+      if(mergedRoot === null) {
+        mergedRoot = i;
+        listPos = mergedRoot;
+        i = i.next;
+      } else {
+        listPos.next = i;
+        i = i.next;
+      }
+    } else {
+      if (mergedRoot === null) {
+        mergedRoot = j;
+        listPos = mergedRoot;
+        j = j.next;
+      } else {
+        listPos.next = j;
+        j = j.next;
+      }
+    }
+  }
+
+  while () {
+
+  }
 };
