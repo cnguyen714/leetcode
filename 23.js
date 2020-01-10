4119831a03b680a0dc74b5dfaae136474e2673e3
Tony Nguyen <calvindnguyen714@gmail.com>
2020-01-09 17:18:59 -0800

    Init 23

---
+++ 23.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/merge-k-sorted-lists/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+
+};
