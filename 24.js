fab29457460d21d7327baa0b2d68bc8cad6f184a
Tony Nguyen <calvindnguyen714@gmail.com>
2020-01-21 22:10:20 -0800

    Solve 24

--- 24.js
+++ 24.js
@@ -10,3 +10,29 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+var swapPairs = function (head) {
+  if (head === null) return null;
+  let right = head;
+  let left = head.next;
+
+  if (left === null) return head;
+  head = left;
+
+  let next = left.next;
+  right.next = next;
+  left.next = right;
+  let prev = right;
+
+  while (next != null) {
+    right = next;
+    left = right.next;
+    if (left === null) return head;
+    prev.next = left;
+    next = left.next;
+    right.next = next;
+    left.next = right;
+    prev = right;
+  }
+
+  return head;
+};
