4a977192c2a4085862a667bb7d1659c9cb0cb1e8
Tony Nguyen <calvindnguyen714@gmail.com>
2020-01-24 22:03:41 -0800

    Work on 4, median of two sorted arr

--- 4.js
+++ 4.js
@@ -9,11 +9,15 @@ var findMedianSortedArrays = function (nums1, nums2) {
   let even = (nums1.length + nums2.length) % 2 === 0; 
   let i = Math.floor(nums1.length / 2);
   let j = Math.floor(nums2.length / 2);
-  let left = 
-  let right =
+  let goalIndex = even ? Math.floor((nums1.length + nums2.length) / 2) - 1 : Math.floor((nums1.length + nums2.length) / 2);
 
   while(left !== right && left ) {
 
   }
 
 };
+
+console.log(findMedianSortedArrays(
+  [3,5,9,10,11,16],
+  [4,6,8,15]
+)) //8.5
