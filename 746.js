d93c4d5b5e8ad982a83783d304692b1f7e8e45d0
Tony Nguyen <calvindnguyen714@gmail.com>
2020-01-27 23:27:52 -0800

    Solve 746

--- 746.js
+++ 746.js
@@ -4,5 +4,14 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
+  let mins = [cost[0], cost[1]];
 
+  for (let i = 2; i < cost.length; i++) {
+    mins.push(mins[i - 2] + cost[i] <= mins[i - 1] + cost[i] ? mins[i - 2] + cost[i] : mins[i - 1] + cost[i]);
+  }
+
+  return Math.min(mins[mins.length - 2], mins[mins.length - 1])
 };
+
+console.log(minCostClimbingStairs([10, 15, 20]));
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]));
