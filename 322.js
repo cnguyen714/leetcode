34cc1986e9466398f6c31b048b05aab621133440
Tony Nguyen <calvindnguyen714@gmail.com>
2020-01-31 21:29:11 -0800

    Solve 322

--- 322.js
+++ 322.js
@@ -5,5 +5,22 @@
  * @return {number}
  */
 var coinChange = function (coins, amount) {
+  let dp = [0];
 
+  for (let j = 1; j <= amount; ++j) {
+    dp.push(Infinity);
+    console.log("\n",j,"");
+    for (let i = 0; i < coins.length; ++i) {
+      if (coins[i] <= j) {
+        console.log(dp[j], " ", dp[j - coins[i]]);
+        dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+      }
+    }
+    console.log("",dp[j])
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount];
 };
+
+// console.log(coinChange([1,2,5],11));
+console.log(coinChange([2,7],13));
