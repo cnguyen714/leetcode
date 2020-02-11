dc4715daf7b027d2a8b6131abc9fdac8faf32a61
Tony Nguyen <calvindnguyen714@gmail.com>
2020-02-11 00:13:07 -0800

    Soln incorrect 654, please premise of max binary tree

--- 654.js
+++ 654.js
@@ -24,23 +24,51 @@ var constructMaximumBinaryTree = function (nums) {
   }
 
   let root = new TreeNode(nums[maxIndex]);
-  let leftIt = new TreeNode(nums[0]);
-  let rightIt = new TreeNode(nums[nums.length - 1]);
 
-  root.left = leftIt;
-  root.right = rightIt;
-
-  for (let i = 1; i < maxIndex; i++) {
-    leftIt.right = new TreeNode(nums[i]);
-    leftIt = leftIt.right;
+  if (maxIndex !== 0) {
+    let tree = maxSubtree(nums, 0, maxIndex - 1);
+    root.left = tree;
   }
 
-  for (let i = nums.length - 2; i > maxIndex; i--) {
-    rightIt.left = new TreeNode(nums[i]);
-    rightIt = rightIt.left;
+  if (maxIndex !== nums.length -1) {
+    let tree = maxSubtree(nums, maxIndex + 1, nums.length - 1);
+    root.right = tree;
   }
 
   return root;
 };
 
-console.log(constructMaximumBinaryTree([3, 2, 1, 6, 0, 5]));
+var maxSubtree = function(nums, i, j) {
+  let root = null;
+  let iter = null;
+  let iterIndex;
+  let next;
+
+  while(i <= j) {
+
+    let next = new TreeNode(nums[nums[i] >= nums[j] ? i : j]);
+    
+    if(iter === null)  {
+      iter = next;
+    } else {
+      if (i === j) {
+        iterIndex > i ? iter.left = next : iter.right = next;
+      } else {
+        if (nums[i] >= nums[j]) {
+          iter.right = next;
+        } else {
+          iter.left = next;
+        }
+      }
+    }
+    if(root === null) root = iter;
+    iterIndex = nums[i] >= nums[j] ? i : j;
+    iter = next;
+    next.val === nums[i] ? i++ : j--;
+  }
+
+  return root;
+}
+
+// console.log(constructMaximumBinaryTree([3, 2, 1, 6, 0, 5]));
+console.log(constructMaximumBinaryTree([3, 2, 1]));
