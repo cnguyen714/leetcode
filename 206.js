03cbcb547f3afa2830ab00901a9609a5d0c5ae8d
Tony Nguyen <calvindnguyen714@gmail.com>
2020-02-20 21:38:30 -0800

    Init 206. Reverse Linked List

---
+++ 206.js
@@ -0,0 +1,16 @@
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+
+};
