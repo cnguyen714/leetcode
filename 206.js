3eba80b27e79da8231ddf1bd6f82b9fbe995df34
Tony Nguyen <calvindnguyen714@gmail.com>
2020-02-20 21:41:34 -0800

    Add test case for 206

--- 206.js
+++ 206.js
@@ -11,6 +11,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+function ListNode(val, next = null) {
+  this.val = val;
+  this.next = next;
+}
+
 var reverseList = function (head) {
 
 };
+
+let head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+let it = head;
+
+while(it) {
+  console.log(it.val);
+  it = it.next;
+}
